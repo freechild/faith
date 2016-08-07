b2a104d8e2b9b03edac991907b67b91881a4835d
JJ <knight09_26@hotmail.com>
2016-08-07 02:27:40 -0700

    total_management vo update

--- src/main/java/com/game/faith/vo/Total_management.java
+++ src/main/java/com/game/faith/vo/Total_management.java
@@ -1,5 +1,33 @@
 package com.game.faith.vo;
 
-public class Total_management {
+import org.springframework.beans.factory.annotation.Autowired;
 
+public class Total_management{
+
+	
+	@Autowired
+	private CharacterVO characterVO;
+	@Autowired
+	private ReligionVO religionVO;
+	public CharacterVO getCharacterVO() {
+		return characterVO;
+	}
+	public void setCharacterVO(CharacterVO characterVO) {
+		this.characterVO = characterVO;
+	}
+	public ReligionVO getReligionVO() {
+		return religionVO;
+	}	
+	public void setReligionVO(ReligionVO religionVO) {
+		this.religionVO = religionVO;
+	}
+	
+	@Override
+	public String toString() {
+		return "Total_management [characterVO=" + characterVO + ", religionVO=" + religionVO + "]";
+	}
+	
+	
+	
+	
 }
